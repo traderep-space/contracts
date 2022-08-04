e7f30d928ab76361b4ae8c8e71fafb9ced12aad7
kiv1n <aloha@kiv1n.ru>
2022-08-04 17:23:16 +0300

    Add functions to verify forecast and get account reputation

--- contracts/Forecast.sol
+++ contracts/Forecast.sol
@@ -11,14 +11,58 @@ contract Forecast is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    mapping(uint256 => address) internal _forecastAuthors;
+    mapping(uint256 => bool) internal _verifiedForecasts;
+    mapping(address => uint256) internal _positiveReputations;
+    mapping(address => uint256) internal _negativeReputations;
+
+    event ReputationUpdate(
+        address account,
+        uint256 positiveReputation,
+        uint256 negativeReputation
+    );
+
     constructor() ERC721("TradeRep Metabolism Forecast", "TRMF") {}
 
+    /**
+     * Mint forecast.
+     */
     function post(string memory tokenURI) public returns (uint256) {
+        // Mint token
         uint256 newItemId = _tokenIds.current();
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
-
+        // Save forecast author
+        _forecastAuthors[newItemId] = msg.sender;
+        // Update counter
         _tokenIds.increment();
         return newItemId;
+    }
+
+    /**
+     * Verify that forecast is true or not true.
+     */
+    function verify(uint256 tokenId) public {
+        require(!_verifiedForecasts[tokenId], "Forecast already verified");
+        // TODO: Implement real verifying
+        // Assume that the forecast was true
+        _verifiedForecasts[tokenId] = true;
+        _positiveReputations[_forecastAuthors[tokenId]] += 1;
+        emit ReputationUpdate(
+            _forecastAuthors[tokenId],
+            _positiveReputations[msg.sender],
+            _negativeReputations[msg.sender]
+        );
+    }
+
+    /**
+     * Get account reputation.
+     */
+    function getReputation(address account)
+        public
+        view
+        returns (uint256, uint256)
+    {
+        return (_positiveReputations[account], _negativeReputations[account]);
     }
 }
--- test/Forecast.ts
+++ test/Forecast.ts
@@ -1,6 +1,5 @@
-import { Signer } from "ethers";
+import { Contract, Signer } from "ethers";
 import { ethers } from "hardhat";
-import { Forecast } from "../typechain-types";
 import { expect } from "chai";
 
 describe.only("Forecast", function () {
@@ -8,7 +7,7 @@ describe.only("Forecast", function () {
   let account1: Signer;
   let account2: Signer;
   // Contract
-  let contract: Forecast;
+  let contract: Contract;
 
   before(async function () {
     // Init accounts
@@ -20,16 +19,29 @@ describe.only("Forecast", function () {
       .then((factory) => factory.deploy());
   });
 
-  it("Should allow to mint multiple tokens", async function () {
+  it("Should mint multiple tokens to one account", async function () {
     await contract
       .connect(account1)
       .post("ipfs://...")
-      .then((tx) => tx.wait());
+      .then((tx: any) => tx.wait());
     await contract
       .connect(account1)
       .post("ipfs://...")
-      .then((tx) => tx.wait());
+      .then((tx: any) => tx.wait());
     let balance = await contract.balanceOf(await account1.getAddress());
     expect(balance).to.equal(2);
+  });
+
+  it("Should verify forecast and change reputation", async function () {
+    await contract.verify(1);
+    let reputation = await contract.getReputation(await account1.getAddress());
+    expect(reputation[0]).to.equal(1);
+    expect(reputation[1]).to.equal(0);
+  });
+
+  it("Should fail if forecast already verified", async function () {
+    await expect(contract.verify(1)).to.be.revertedWith(
+      "Forecast already verified"
+    );
   });
 });
