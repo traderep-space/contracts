61275b29789dba6e95e7cd7661838e3d69bd353a
kiv1n <aloha@kiv1n.ru>
2022-09-12 13:14:03 +0300

    Add support of many chains in the deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,25 @@
 import { ethers } from "hardhat";
 import hre from "hardhat";
 
-const deployedContracts = {
-  forecast: "0xf4AE34F436e34e7c69440869fae22F5ec862c265",
-  bio: "0x5d776915F06958434E0C05eC75194FDC5f759548",
-  earlyAdopter: "0xC3Ce59992D266b0ADB985Bf6De78b2d5A3191beB",
+const deployedContracts: any = {
+  mumbai: {
+    forecast: "0xf4AE34F436e34e7c69440869fae22F5ec862c265",
+    bio: "0x5d776915F06958434E0C05eC75194FDC5f759548",
+    earlyAdopter: "0xC3Ce59992D266b0ADB985Bf6De78b2d5A3191beB",
+  },
 };
 
 async function main() {
+  // Define chain
   const chain = hre.hardhatArguments.network;
-
+  if (!chain) {
+    console.log("Chain is not defined");
+    return;
+  }
   console.log("Running on chain: " + chain);
+
+  // Define deployed contracts by chain
+  const chainDeployedContracts = deployedContracts[chain];
 
   // Deploy forecast contract
   if (!deployedContracts.forecast) {
